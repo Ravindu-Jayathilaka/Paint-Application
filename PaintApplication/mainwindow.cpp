c1008ee6486d33127d1f01c677b9d3d0ecfa5f0d
Ravindu Jayathilaka <57854539+Ravindu-Jayathilaka@users.noreply.github.com>
2022-01-12 23:51:23 +0530

    Update mainwindow.cpp

--- PaintApplication/mainwindow.cpp
+++ PaintApplication/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtWidgets>
+
+#include "scribblearea.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,6 +13,55 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    scribbleArea = new ScribbleArea;
+    setCentralWidget(scribbleArea);
+    createActions();
+    createMenus();
+    setWindowTitle(tr("Scribble"));
+    resize(500,500);
+
 }
+
+void MainWindow::closeEvent(QCloseEvent *event){
+    if(maybeSave()){
+        event->accept();
+    }else{
+        event->ignore();
+    }
+}
+
+void MainWindow::open(){
+    if(maybeSave()){
+        QString fileName=QFileDialog::getOpenFileName(this,tr("Open File"),QDir::currentPath());
+        if(!fileName.isEmpty()){
+            scribbleArea->openImage(fileName);
+        }
+    }
+}
+
+void MainWindow::save(){
+    QAction *action= qobject_cast<QAction *>(sender());
+    QByte fileFormat = action->data().toByteArray();
+    saveFile(fileFormat);
+}
+
+void MainWindow::penColor(){
+    QColor newColor = QColorDialog::getColor(scribbleArea->penColor());
+    if(newColor.isValid()){
+        scribbleArea->setPenColor(newColor);
+    }
+}
+
+void MainWindow::penWidth(){
+    bool ok;
+    int newWidth = QInputDialog::getInt(this,tr("Scribble"),tr("Select pen width : "),scribbleArea->penWidth(),1, 50, 1, &ok);
+    if(ok){
+        scribbleArea->setPenWidth(newWidth);
+    }
+}
+
+void MainWindow::about(){
+    QMessageBox::about(this,tr("About Scribble"),tr("<p>The <b>Scribble</b> example is awesome</p>"));
+}
+
 
