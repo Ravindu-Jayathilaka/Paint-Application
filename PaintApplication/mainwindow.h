f1240de2d86c7d2a56b7e0493919c2906666e99d
Ravindu Jayathilaka <57854539+Ravindu-Jayathilaka@users.noreply.github.com>
2022-01-15 22:35:21 +0530

    bug fixes

--- PaintApplication/mainwindow.h
+++ PaintApplication/mainwindow.h
@@ -6,21 +6,18 @@
 
 class ScribbleArea;
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow();
 
 protected:
+    // Function used to close an event
     void closeEvent(QCloseEvent *event) override;
 
+// The events that can be triggered
 private slots:
     void open();
     void save();
@@ -29,16 +26,29 @@ private slots:
     void about();
 
 private:
+    // Will tie user actions to functions
     void createActions();
     void createMenus();
+
+    // Will check if changes have occurred since last save
     bool maybeSave();
+
+    // Opens the Save dialog and saves
     bool saveFile(const QByteArray &fileFormat);
+
+    // What we'll draw on
     ScribbleArea *scribbleArea;
+
+    // The menu widgets
     QMenu *saveAsMenu;
     QMenu *fileMenu;
     QMenu *optionMenu;
     QMenu *helpMenu;
+
+    // All the actions that can occur
     QAction *openAct;
+
+    // Actions tied to specific file formats
     QList<QAction *> saveAsActs;
     QAction *exitAct;
     QAction *penColorAct;
@@ -47,6 +57,6 @@ private:
     QAction *clearScreenAct;
     QAction *aboutAct;
     QAction *aboutQtAct;
-    Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
+
+#endif
